d39b4a401b1dbe455eebc6d769c216bdc12f1e4a
Kjoobin <imkj1148@gmail.com>
2020-06-14 18:35:23 +0900

    Add setting modal

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Modal, Button, TouchableWithoutFeedback } from 'react-native';
 import styled from 'styled-components/native';
 
 import OsView from '../components/OsView';
@@ -66,6 +67,10 @@ const FAKEDATA_1 = {
     ]
 }
 
+const Wrapper = styled.View`
+    flex:1;
+`;
+
 const TitleView = styled.View`  
     justify-content:center;
     align-items:center;
@@ -106,43 +111,110 @@ const QuestionCardWrapper = styled.View`
     padding:10px 0px;
 `;
 
+const ModalWrapper = styled.View`
+    flex:1;
+    justify-content:center;
+    align-items:center;
+    background-color:rgba(112,112,112,0.5);
+    padding:0px 8px;
+`;
+
+const ModalLayout = styled.View`
+    background-color:${({ theme }: ThemeProps): string => theme.white};
+    align-items:center;
+    padding:18px;
+    width:100%;
+    height:70%;
+`;
+
+const ModalTitle = styled.Text`
+    font-size:20px;
+    color:#DEE53A;
+    margin-bottom:25px;
+`;
+
+const ModalTab = styled.View`
+    width:100%;
+    height:64px;
+    padding-left:16px;
+    justify-content:center;
+    box-shadow:0px 3px 6px #000;
+    background-color:white;
+    elevation:6;
+    margin-bottom:7px;
+`;
+
+const ModalTabText = styled.Text`
+    font-size:12px;
+    color:${({ theme }: ThemeProps): string => theme.textColor};
+`;
 
 const Profile = () => {
     const [select, setSelect] = useState<select>("card");
+    const [modal, setModal] = useState<boolean>(false);
 
     const handleSelect = (text: select) => () => {
         setSelect(text);
+    }
+
+    const handleModal = () => {
+        setModal(!modal);
     }
 
     const cards: card[] | [] = FAKEDATA_1[select];
 
     return (
         <OsView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-            <ScrollView>
-                <TitleView>
-                    <Title>Question</Title>
-                </TitleView>
-                <BodyWrapper>
-                    <UserCard {...FAKEDATA} />
-                    <SelectWrapper>
-                        <Select onPress={() => handleSelect('card')()}>
-                            <SelectView style={{ borderBottomWidth: (select === "card" ? 3 : 0) }}>
-                                <SelectText style={{ color: (select === "card" ? theme.textColor : "black") }}>Card</SelectText>
-                            </SelectView>
-                        </Select>
-                        <Select onPress={() => handleSelect('question')()}>
-                            <SelectView style={{ borderBottomWidth: (select === "question" ? 3 : 0) }}>
-                                <SelectText style={{ color: (select === "question" ? theme.textColor : "black") }}>Question</SelectText>
-                            </SelectView>
-                        </Select>
-                    </SelectWrapper>
-                    {cards.map((item) =>
-                        <QuestionCardWrapper key={item.id}>
-                            <QuestionCard {...item} />
-                        </QuestionCardWrapper>
-                    )}
-                </BodyWrapper>
-            </ScrollView>
+            <Wrapper>
+                <Modal visible={modal} transparent={true} >
+                    <TouchableWithoutFeedback onPress={() => setModal(false)}>
+                        <ModalWrapper>
+                            <TouchableWithoutFeedback>
+                                <ModalLayout>
+                                    <ModalTitle>Helle {FAKEDATA.username}</ModalTitle>
+                                    <ModalTab>
+                                        <ModalTabText>Username</ModalTabText>
+                                    </ModalTab>
+                                    <ModalTab>
+                                        <ModalTabText>Password</ModalTabText>
+                                    </ModalTab>
+                                    <ModalTab>
+                                        <ModalTabText>Text size</ModalTabText>
+                                    </ModalTab>
+                                    <ModalTab>
+                                        <ModalTabText>Dark theme</ModalTabText>
+                                    </ModalTab>
+                                </ModalLayout>
+                            </TouchableWithoutFeedback>
+                        </ModalWrapper>
+                    </TouchableWithoutFeedback>
+                </Modal>
+                <ScrollView>
+                    <TitleView>
+                        <Title>Question</Title>
+                    </TitleView>
+                    <BodyWrapper>
+                        <UserCard {...FAKEDATA} onPress={handleModal} />
+                        <SelectWrapper>
+                            <Select onPress={() => handleSelect('card')()}>
+                                <SelectView style={{ borderBottomWidth: (select === "card" ? 3 : 0) }}>
+                                    <SelectText style={{ color: (select === "card" ? theme.textColor : "black") }}>Card</SelectText>
+                                </SelectView>
+                            </Select>
+                            <Select onPress={() => handleSelect('question')()}>
+                                <SelectView style={{ borderBottomWidth: (select === "question" ? 3 : 0) }}>
+                                    <SelectText style={{ color: (select === "question" ? theme.textColor : "black") }}>Question</SelectText>
+                                </SelectView>
+                            </Select>
+                        </SelectWrapper>
+                        {cards.map((item) =>
+                            <QuestionCardWrapper key={item.id}>
+                                <QuestionCard {...item} />
+                            </QuestionCardWrapper>
+                        )}
+                    </BodyWrapper>
+                </ScrollView>
+            </Wrapper>
         </OsView>
     )
 }
