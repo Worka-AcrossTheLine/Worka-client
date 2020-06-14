9cca54c69122aa90496244ae0ab5eeae2e2cc959
Kjoobin <imkj1148@gmail.com>
2020-06-14 16:49:40 +0900

    Add shadow box controll props

--- src/components/ShadowBox.tsx
+++ src/components/ShadowBox.tsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import styled from 'styled-components/native'
 
 type Props = {
-    children: React.ReactChild
+    children: React.ReactChild;
+    semi?: "top" | "bottom";
 }
 
 const Wrapper = styled.View`
     width:100%;
     height:151.82px;
     border-radius:8px;
-    padding:26px 20px;
     background-color:#FFFFFF
     box-shadow:0px 3px 6px #000;
     elevation:6;
 `;
 
-export default function ShadowBox({ children }: Props) {
+export default function ShadowBox({ semi, children }: Props) {
+    const style = semi ? (
+        semi === "top" ?
+            { width: "100%", height: 64, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, marginBottom: Platform.OS === 'android' ? 2 : 0 }
+            :
+            { width: "100%", height: 87, borderTopLeftRadius: 0, borderTopRightRadius: 0, marginBottom: Platform.OS === 'android' ? 2 : 0 })
+        :
+        {}
     return (
-        <Wrapper>
+        <Wrapper style={style}>
             {children}
         </Wrapper>
     )
