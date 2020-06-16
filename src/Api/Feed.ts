78b203e521f7a7f2823b0b3b48f271831489ee5e
Kjoobin <imkj1148@gmail.com>
2020-06-16 13:23:26 +0900

    Modify base url to local

--- src/Api/Feed.ts
+++ src/Api/Feed.ts
@@ -1,14 +1,14 @@
-import axios, {AxiosError, AxiosPromise} from "axios";
+import axios, { AxiosError, AxiosPromise } from "axios";
 
 const reqresApi = axios.create({
-    baseURL: 'http://172.30.1.13:8000/api/v1/post',
+    baseURL: 'http://127.0.0.1:8000/api/v1/post',
 });
 
 export const getFeed = () => {
     return reqresApi.get(`/all/`)
-    .catch((error: AxiosError) => {
-        throw error.response
-    });
+        .catch((error: AxiosError) => {
+            throw error.response
+        });
 };
 
 
