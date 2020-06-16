cbbb8a45ca1c10af70f8cd09887cabdfc57db7a2
hong <snh1003@naver.com>
2020-06-16 09:54:13 +0900

    TabCard.tsx(Add camera roll)

--- src/screens/TabCreate/TabCard.tsx
+++ src/screens/TabCreate/TabCard.tsx
@@ -1,6 +1,9 @@
 import React, { useState }from 'react'
-import { Keyboard, TouchableWithoutFeedback } from 'react-native'
+import { Keyboard, TouchableWithoutFeedback, Text, View, Image } from 'react-native'
 import styled from 'styled-components/native'
+import Constants from 'expo-constants';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 import MakeJobTagInput from "../../components/MakeJobTagInput"
 import MakeInterestingInput from "../../components/MakeInterestingInput"
@@ -8,6 +11,7 @@ import MakeButton from "../../components/MakeButton"
 import CancerButton from '../../components/CancerButton'
 import OsView from "../../components/OsView"
 import addTap from "../../constants/addTap"
+import {Avatar} from "react-native-elements";
 
 
 const Wrapper = styled.SafeAreaView`
@@ -31,15 +35,63 @@ const InputWrapper = styled.View`
   
   flex-direction:column;
 `
+interface Cards {
 
-const TabCard = () => {
+}
+const TabCard: React.FC = (props) => {
 
     const [tapTag, setTaptag] = useState('');
     const [InterestingTitle, setInterestingTitle] = useState('');
-    
+    const [image, setImage] = useState('');
+
     const handleKeyboard  = () => {
         Keyboard.dismiss();
     }
+    const camera = async (arg:void) => {
+        try {
+            // @ts-ignore
+            if (Constants.platform.ios) {
+                const { status } = await Permissions.askAsync(Permissions.CAMERA);
+                if (status !== 'granted') {
+                    alert("카메라 허가 필요");
+                    return;
+                }
+            }
+
+            const result = await ImagePicker.launchCameraAsync();
+            if (!result.cancelled) {
+                setImage(result.uri)
+            }
+        } catch (e) {
+            console.log(e);
+            alert("업로드에러");
+        }
+    }
+
+    const pickImage = async () => {
+        try {
+            // @ts-ignore
+            if (Constants.platform.ios) {
+                const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+                if (status !== 'granted') {
+                    alert("카메라 허가 필요");
+                    return;
+                }
+            }
+            let result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+            });
+            if (!result.cancelled) {
+                setImage(result.uri)
+            }
+        }catch(e){
+            console.log(e);
+            alert("카메라 라이브러리 에러");
+        }
+    }
+
+
     return (
         <OsView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
             <Wrapper>
@@ -51,13 +103,12 @@ const TabCard = () => {
                     <MakeButton title="MAKE"></MakeButton>
                 </TitleWrapper>
                 <InputWrapper>
-                    <MakeJobTagInput 
+                    <MakeJobTagInput
                         placeholder="Make Job Tag"
                         value={tapTag}
                         onChange = {addTap(setTaptag)}
                         autoFocus = { true }
                         onPress={handleKeyboard}
-                        
                     />
                     <MakeInterestingInput
                         placeholder="Make Interesting Title"
@@ -65,9 +116,27 @@ const TabCard = () => {
                         onChange={addTap(setInterestingTitle)}
                         autoFocus = {true}
                     />
-            </InputWrapper>
-        </Wrapper>
-    </OsView>
+                    <View style={{ }}>
+                        <View style={{ margin: 20 }}>
+
+                            <Avatar
+                                size="medium"
+                                title="C"
+                                onPress={camera}
+                                source={{ }}
+                            />
+                            <Avatar
+                                size="medium"
+                                title="D"
+                                onPress={pickImage}
+                                source={{ }}
+                            />
+                            <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
+                        </View>
+                    </View>
+                </InputWrapper>
+            </Wrapper>
+        </OsView>
     )
 }
 const FlexWrapper = styled.View`
