fa815b2468f3b20129d6839f3e4e89f306b6e04b
Kjoobin <imkj1148@gmail.com>
2020-06-19 00:30:13 +0900

    Add tab card wrapper animation

--- src/components/MakeCardDescriptionInput.tsx
+++ src/components/MakeCardDescriptionInput.tsx
@@ -7,47 +7,44 @@ type Props = {
     value: string;
     onChange: (e: string) => void;
     autoFocus?: boolean;
-    onBlur:() => void;
-    onFocus:() => void;
+    onBlur: () => void;
+    onFocus: () => void;
     keyboardType?: 'default';
     multiline: true;
     numberOfLines: {}
-}
-
-const MakeCardDescriptionInput = ({
-                                  placeholder, value, onChange, autoFocus = false, keyboardType, onBlur, onFocus, multiline, numberOfLines
-                              }:Props) => {
-    let textAlign: 'left'
-    if(value.length>0) {
-        textAlign = 'left'
-    }
-    return (
-        <InputWrapper>
-            <Input
-                multiline = {multiline}
-                numberOfLines = {4}
-                placeholder={placeholder}
-                onChangeText={(e) => onChange(e)}
-                value = {value}
-                onBlur = {onBlur}
-                onFocus = {onFocus}
-            >
-            </Input>
-
-        </InputWrapper>
-    )
 }
 
 const InputWrapper = styled.View`
     box-shadow:0px 3px 6px #000;
     font-size: 16px;
     margin-bottom: 5px;
-    
 `
 
 const Input = styled.TextInput`
+    height:100%;
     background-color:white;
     padding: 21px 10px 21px 43px;
 `
+
+const MakeCardDescriptionInput = ({
+    placeholder,
+    value,
+    onChange,
+    onBlur,
+    onFocus,
+    multiline
+}: Props) =>
+    <InputWrapper>
+        <Input
+            multiline={multiline}
+            numberOfLines={4}
+            placeholder={placeholder}
+            onChangeText={(e) => onChange(e)}
+            value={value}
+            onBlur={onBlur}
+            onFocus={onFocus}
+        >
+        </Input>
+    </InputWrapper>
 
 export default MakeCardDescriptionInput
