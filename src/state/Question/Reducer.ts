5aed5db01831f33cc847418686149ef40935f8c3
Kjoobin <imkj1148@gmail.com>
2020-06-18 20:59:55 +0900

    Add type

--- src/state/Question/Reducer.ts
+++ src/state/Question/Reducer.ts
@@ -1,4 +1,4 @@
-import {Action} from "../index";
+import { Action } from "../index";
 import {
     GET_QUESTION_FAIL,
     GET_QUESTION_REQUEST,
@@ -8,13 +8,24 @@ import {
     MAKE_QUESTION_SUCCESS
 } from "./Action";
 
+export interface questionCard {
+    id: string;
+    desc: string;
+    image: string;
+    tags: string[];
+    questions: string[];
+    author: {
+        username: string;
+    }
+}
+
 export interface QuestionState {
     fetching: boolean;
-    data: any;
+    data: questionCard[];
     err: any;
 }
 
-const initialState: QuestionState = {fetching: false, data: null, err: null};
+const initialState: QuestionState = { fetching: false, data: null, err: null };
 
 export const questionFeed = (state: QuestionState = initialState, action: Action) => {
     switch (action.type) {
