19b1004aec38c91712a4b9f2587ae6851e7e76a4
Kjoobin <imkj1148@gmail.com>
2020-06-18 21:00:17 +0900

    Add on search feature

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,9 +3,10 @@ import { takeLatest } from 'redux-saga/effects';
 
 import login, { LoginState } from './login';
 import signup, { SignupState } from './signup'
-import {getFeed, FeedState} from '../state/Feed/Reducer'
+import { getFeed, FeedState } from '../state/Feed/Reducer'
 import setting, { SettingState } from './setting'
-import {QuestionState, questionFeed} from "../state/Question/Reducer";
+import { search, SearchState } from '../state/Search/Reducer'
+import { QuestionState, questionFeed } from "../state/Question/Reducer";
 import {
     ProfileState,
     ProfileFeed,
@@ -20,11 +21,21 @@ export type RootState = {
     signup: SignupState,
     feed: FeedState,
     setting: SettingState,
-    question : QuestionState
-    profile : ProfileState
-    profileQuestion : ProfileQuestionState
-    questionComment : QuestionCommentState
+    search: SearchState,
+    question: QuestionState,
+    profile: ProfileState,
+    profileQuestion: ProfileQuestionState,
+    questionComment: QuestionCommentState,
 }
 
-export default combineReducers({ login, signup, feed: getFeed, setting, question: questionFeed,
-    profile: ProfileFeed, profileQuestion : ProfileQuestionFeed ,questionComment : QuestionCommentFeed });
+export default combineReducers({
+    login,
+    signup,
+    feed: getFeed,
+    setting,
+    search,
+    question: questionFeed,
+    profile: ProfileFeed,
+    profileQuestion: ProfileQuestionFeed,
+    questionComment: QuestionCommentFeed,
+});
