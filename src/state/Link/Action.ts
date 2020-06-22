54a0103a269882640fc83e0251c8167bab4e95b9
Kjoobin <imkj1148@gmail.com>
2020-06-22 19:56:53 +0900

    Add back when make feed

--- src/state/Link/Action.ts
+++ src/state/Link/Action.ts
@@ -20,13 +20,13 @@ interface postLink {
 export interface Links {
     id: number,
     author: {
-        pk:number,
+        pk: number,
         username: string,
         user_image: string
     },
     title: string,
     tags: string[],
-    created_at:  string
+    created_at: string
 }
 
 
@@ -40,6 +40,7 @@ export const GET_LINK_DETAIL_REQUEST = 'GET_LINK_DETAIL_REQUEST';
 export const GET_LINK_DETAIL_SUCCESS = 'GET_LINK_DETAIL_SUCCESS';
 export const GET_LINK_DETAIL_FAIL = 'GET_LINK_DETAIL_FAIL';
 // MAKE LINK
+export const MAKE_LINK_INIT = 'MAKE_LINK_INIT';
 export const MAKE_LINK_REQUEST = 'MAKE_LINK_REQUEST';
 export const MAKE_LINK_SUCCESS = 'MAKE_LINK_SUCCESS';
 export const MAKE_LINK_FAIL = 'MAKE_LINK_FAIL';
@@ -49,29 +50,29 @@ export const GET_LINKTAG_SUCCESS = 'GET_LINKTAG_SUCCESS';
 export const GET_LINKTAG_FAIL = 'GET_LINKTAG_FAIL';
 
 export const getLinkRequest = () => {
-    return {type:GET_LINK_REQUEST};
+    return { type: GET_LINK_REQUEST };
 };
 export const getLinkSuccess = (data: postLink) => {
-    return {type:GET_LINK_SUCCESS, payload:{data}};
+    return { type: GET_LINK_SUCCESS, payload: { data } };
 };
 export const getLinkFail = (err: boolean) => {
-    return {type:GET_LINK_FAIL, payload: {err}};
+    return { type: GET_LINK_FAIL, payload: { err } };
 }
 export const getLinkDetailRequest = (id: string) => {
-  return {type: GET_LINK_DETAIL_REQUEST, payload: {id}}
+    return { type: GET_LINK_DETAIL_REQUEST, payload: { id } }
 }
 export const getLinkDetailSuccess = (detail: postLink[]) => {
-    return {type: GET_LINK_DETAIL_SUCCESS, payload: {detail}}
+    return { type: GET_LINK_DETAIL_SUCCESS, payload: { detail } }
 }
 export const getLinkDetailFail = (err: boolean) => {
-    return {type: GET_LINK_DETAIL_FAIL, payload: {err}}
+    return { type: GET_LINK_DETAIL_FAIL, payload: { err } }
 }
 export const makeLinkRequest = (data: getLink) => {
-    return {type: MAKE_LINK_REQUEST, payload: {data}}
+    return { type: MAKE_LINK_REQUEST, payload: { data } }
 }
 export const makeLinkSuccess = (data: getLink[]) => {
-    return {type: MAKE_LINK_SUCCESS, paylaod: {data}}
+    return { type: MAKE_LINK_SUCCESS, paylaod: { data } }
 }
 export const makeLinkFail = (err: boolean) => {
-    return {type:MAKE_LINK_REQUEST, payload: {err}}
+    return { type: MAKE_LINK_REQUEST, payload: { err } }
 }
