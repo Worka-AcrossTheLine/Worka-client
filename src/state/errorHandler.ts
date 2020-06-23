c1537aee241223f9d9ece39b2781a39e4f6b54ce
hong <snh1003@naver.com>
2020-06-23 18:55:08 +0900

    Add more error

--- src/state/errorHandler.ts
+++ src/state/errorHandler.ts
@@ -7,5 +7,13 @@ export const errorHandler = (err:number):void => {
         alert('페이지가 존재하지 않습니다.')
     }else if (err === 413) {
         alert('파일이 너무 큽니다')
+    }else if (err === 403) {
+        alert('접근권한이 없습니다')
+    }else if (err === 405) {
+        alert('잘못된 메서드로 요청하였습니다')
+    }else if (err === 408) {
+        alert('서버의 응답이 없습니다(Timeout)')
+    }else if (err === 503) {
+        alert('서버가 과부화중')
     }
 }
