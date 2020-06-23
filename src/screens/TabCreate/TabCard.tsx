61fe50ef2a2138e69c61ca5d7d9819862dd3dad1
hong <snh1003@naver.com>
2020-06-23 18:05:17 +0900

    change card upload(Taptag)

--- src/screens/TabCreate/TabCard.tsx
+++ src/screens/TabCreate/TabCard.tsx
@@ -159,7 +159,7 @@ const TabCard = ({ navigation }: Props) => {
         if (token) {
             dispatch({
                 type: MAKE_FEED_REQUEST,
-                payload: { title: InterestingTitle, tags: tapTag.replace(/,/gi, '').replace(/\s/gi, ','), text: Description, images: image, token: token }
+                payload: { title: InterestingTitle, tags: tapTag.replace(/,/gi, '').split(' '), text: Description, images: image, token: token }
             })
         } else {
         }
